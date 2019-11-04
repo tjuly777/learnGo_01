afaca80f91ad00a3ec2e6f6bff0e934c21d5fa2c
tjuly777 <tjuly777@gmail.com>
2019-11-04 23:37:28 +0900

    複数の返り値を返す関数を追加

--- main.go
+++ main.go
@@ -8,7 +8,16 @@ import (
 func main() {
 	fmt.Println(AppName(2, 5))
 
+	q, r := getMultiReturn(7, 3)
+	fmt.Printf("Quotient = %v, Remainder = %v\n", q, r)
+
 	fmt.Println(animals.ElephantFeed())
 	fmt.Println(animals.MonkeyFeed())
 	fmt.Println(animals.RabbitFeed())
+}
+
+func getMultiReturn(a, b int) (int, int) {
+	q := a / b
+	r := a % b
+	return q, r
 }
