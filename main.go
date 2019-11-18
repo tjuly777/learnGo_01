3acb5ee37fcb987fff2acb273de2fd2bb3dad38e
tjuly777 <tjuly777@gmail.com>
2019-11-19 08:09:44 +0900

    ・main.goの関数のいくつかを、functions.goへ移動 ・コメントを追記

--- main.go
+++ main.go
@@ -1,68 +1,17 @@
 package main
 
 import (
-	"./animals"
+	"./animals" //他パッケージの読み込み
 	"fmt"
 )
 
 func main() {
+	//同パッケージの関数
 	fmt.Println(AppName(2, 5))
 
+	//他パッケージの関数
 	fmt.Println(animals.ElephantFeed())
 	fmt.Println(animals.MonkeyFeed())
 	fmt.Println(animals.RabbitFeed())
-
-	//複数の戻り値を取得
-	q1, r1 := getMultiReturn(7, 3)
-	fmt.Printf("Quotient = %v, Remainder = %v\n", q1, r1)
-
-	//_を使えば、戻り値の一部を破棄できる
-	q2, _ := getMultiReturn(7, 3)
-	fmt.Printf("Quotient = %v, Remainder = null\n", q2)
-
-	/**
-	エラー処理の慣例的な書き方。複数の戻り地の一部に、本来nilを採る変数errを充てる。
-	errがnilでないことで、エラーかどうかを検知する。
-	*/
-	result, err := getSomeError()
-	if err != nil {
-		fmt.Printf("Result == %v. Error Cause '%v'\n", result, err)
-	}
-
-	//変数に無名関数を代入することも可能。
-	f := func(x, y int) int { return x + y }
-	fmt.Println(f(1, 11))
-
-	//関数を返す関数
-	f2 := returnFunc()
-	f2()
-	//変数を経由せずに直接呼び出すことも可能。
-	returnFunc()()
-
-	//関数を引数としてとる。ここでは、無名関数を引数として与えている。
-	callFunction(func() {
-		fmt.Println("Called Function.")
-	})
-
-	//クロージャから関数内のローカル変数にアクセスした際の挙動確認
-	f4 := later()
-	fmt.Println(f4("It"))
-	fmt.Println(f4("is"))
-	fmt.Println(f4("Closure Lesson."))
-
-	//上記を応用した、ジェネレータ
-	f5 := incrementFunc()
-	fmt.Println(f5())
-	fmt.Println(f5())
-	fmt.Println(f5())
-	fmt.Println(f5())
-
-	/**
-	新たにクロージャを生成すると、値は別個となる。
-	オブジェクトのインスタンス化に近いイメージ！
-	*/
-	f5_1 := incrementFunc()
-	fmt.Println(f5_1())
-	fmt.Println(f5())
 
 }
